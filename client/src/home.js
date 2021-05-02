4eb272f8e0bf3dd968ce0ddff97c040b3295254e
js-junior <arnabgogoi83@gmail.com>
2021-05-02 14:39:14 +0530

    q

--- client/src/home.js
+++ client/src/home.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect, useRef } from 'react'
 import { Avatar, Button, Input } from '@material-ui/core'
 import { useParams, useHistory } from 'react-router-dom'
+import FacebookLogin from 'react-facebook-login'
 
 function Home(){
 
-
+ 
 	return(
 		<>
-<h1> Hello there, building tik-tok clone</h1>
+
+<h1> Hello there, building whatsapp clone🚀</h1>
 		</>
 	)
 }
