ee0ed338bc4aa39c4c04a9f77ecf338f21d9aba2
js-junior <arnabgogoi83@gmail.com>
2021-06-01 18:23:19 +0530

    bug fixes 3

--- client/src/room.js
+++ client/src/room.js
@@ -7,22 +7,58 @@ import { State } from './state.js'
 
 function Rooms(){
 
-        const [ rooms, setRooms ] = useState([])
+        const [ rooms, setRooms ] = useState([1,2,3])
+	const [ user, setUser ] = useState([])
         const history = useHistory()
 	const { id } = useParams()
-        const [{ base_url, username }, dispatch ] = useContext(State)
+        const [{ base_url }, dispatch ] = useContext(State)
 	const [ term, setTerm ] = useState("")
         const token = localStorage.getItem('jwt')
         const user_id = jwt_decode(token)
 
+	useEffect(()=>{
+	fetch(`${base_url}/user/${user_id._id}`,{
+                                method: 'GET',
+                                headers: { authorization: 'bearer ' + token }
+                        })
+                        .then(res =>{
+                                return res.json()
+                        })
+                        .then(data =>{
+                                setUser(data.message)
+                                console.log(data.message)
+                        })
+                        .catch(err =>{
+                                console.log(err)
+                        })
+	},[])
+
         return(
                 <>
-		<AppBar>
-		<Toolbar><Typography variant='h6'>{username}</Typography></Toolbar>
-
-		<input className='search_input' value={term}
+		<Typography variant='h6'>{user.username}</Typography>{/*
+		<Avatar className='room_avatar' src={`${base_url}/${user.photo}`} />*/}
+		<AppBar position='sticky' color='white'>
+		<input className='search_input' value={term} placeholder='search'
 		onChange={ (e)=>{ setTerm(e.target.value) }} />
-		</AppBar><Toolbar />
+		<Avatar className='room_avatar' src={`${base_url}/${user.photo}`} />
+			</AppBar><Toolbar />
+
+		{rooms.map((val,index)=>{
+			return(
+				<>
+		<div className='notification_bar'>
+        <Avatar className='notification_text' src={`${base_url}/${user.photo}`}
+                id='notification_avatar'  />
+                <Typography variant='subtitle5'>
+                <Typography className='notification_text' id='notification_text'
+                variant='h6'> {user.username} </Typography><br /> Hello there
+                </Typography>
+                </div>
+				</>
+			)
+		})}
+
+		
                 </>
         )
 }
